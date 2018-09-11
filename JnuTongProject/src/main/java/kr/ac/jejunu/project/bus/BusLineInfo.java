d1bf739df815399c13cc4fe87f8f7bd2b9282c20
song <song@localhost.localdomain>
2018-09-11 13:22:27 +0900

    ..

--- JnuTongProject/src/main/java/kr/ac/jejunu/project/bus/BusLineInfo.java
+++ JnuTongProject/src/main/java/kr/ac/jejunu/project/bus/BusLineInfo.java
@@ -2,10 +2,41 @@ package kr.ac.jejunu.project.bus;
 
 import lombok.Data;
 
-@Data
 public class BusLineInfo {
     private String lineId;
     private String lineNo;
     private String detailLineNo;
     private String description;
+
+    public String getLineId() {
+        return lineId;
+    }
+
+    public void setLineId(String lineId) {
+        this.lineId = lineId;
+    }
+
+    public String getLineNo() {
+        return lineNo;
+    }
+
+    public void setLineNo(String lineNo) {
+        this.lineNo = lineNo;
+    }
+
+    public String getDetailLineNo() {
+        return detailLineNo;
+    }
+
+    public void setDetailLineNo(String detailLineNo) {
+        this.detailLineNo = detailLineNo;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
